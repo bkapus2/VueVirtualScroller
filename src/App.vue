5969c43c305158f5ac027ce67a37b99e1f3eafbe
Unknown <brian.kupi@gmail.com>
2018-02-01 19:34:18 -0600

    added height updates, started #updateVisibleItems

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ window.oncePerFrame = oncePerFrame;
 const randomString = () => Math.random().toString(36).substring(7);
 const randomInt = () => Math.ceil(Math.random()*100);
 const randomFloat = () => Math.random()*10;
-const items = Array.apply(null, Array(1000)).map(()=>({
+const items = Array.apply(null, Array(100000)).map(()=>({
   col1: randomString(),
   col2: randomInt(),
   col3: randomFloat(),
--- src/components/RecyclingVirtualScroller.vue
+++ src/components/RecyclingVirtualScroller.vue
@@ -20,13 +20,43 @@ function validateItems(items) {
   return items.some(validateItem)
 }
 
-class ComponentInstancePool {
-  constructor() {
-    this.pools = [];
+function findUnusedOfType(type) {
+  return function findFunc(entry) {
+    return entry.type === type && entry.used === false;
   }
-  // getUnusedComponentInstance(component) {
-  //   let pool 
-  // }
+}
+function getComponentInstancePool() {
+  const pool = [];
+  let uid = 1;
+  return {
+    getInstance({type, index, data}) {
+      let entry = pool.find(findUnusedOfType(type));
+      if (!entry) {
+        entry = {
+          id: uid++,
+          top: 0,
+          index,
+          data,
+          used: true
+        }
+      } else {
+        Object.assign(entry, {
+          index,
+          data,
+          used: true
+        });
+      }
+      return entry;
+    },
+    releaseInstance(entry) {
+      Object.assign(entry, {
+        top: 0,
+        index: null,
+        data: null,
+        used: false,
+      });
+    }
+  };
 }
 
 export default {
@@ -34,12 +64,20 @@ export default {
     itemHeight: {
       required: false,
       type: Number,
-      default: null,
+      default: 20, // todo: change back to null when done developing
     },
     items: {
       required: true,
       type: Array,
       validateItems
+    },
+    buffer: {
+      required: false,
+      type: Number,
+      default: 100,
+      validator(buffer) {
+        return buffer > 0;
+      }
     }
   },
   data() {
@@ -50,6 +88,7 @@ export default {
   },
   computed: {
     containerStyle() {
+      console.log('updating container style')
       return {
         height: this.totalHeight+'px',
       }
@@ -64,12 +103,56 @@ export default {
   created() {
     this.onScroll = oncePerFrame(this.onScroll);
   },
+  mounted() {
+    this.updateVisibleItems();
+  },
+  beforeUpdate() {
+    window.console.time('update');
+  },
+  updated() {
+    window.console.timeEnd('update');
+  },
   methods: {
     onScroll() {
       console.log(new Date().valueOf())
+      this.updateVisibleItems();
+    },
+    updateVisibleItems() {
+      const { startIndex, endIndex, totalHeight } = this.fixedHeightMode
+        ? this.getFixedHeightData()
+        : this.getVariableHeightData();
+      console.log({ startIndex, endIndex, totalHeight })
+
+      this.totalHeight = totalHeight;
+    },
+    getBounds() {
+      const { scrollTop, offsetHeight } = this.$el;
+      return {
+        top: scrollTop,
+        bottom: scrollTop + offsetHeight,
+      };
+    },
+    getFixedHeightData() {
+      const buffer = this.buffer;
+      const bounds = this.getBounds();
+      const topBoundary = bounds.top - buffer;
+      const bottomBoundary = bounds.bottom + buffer;
+      const items = this.items;
+      const length = items.length;
+      const itemHeight = this.itemHeight;
+      const startIndex = Math.floor(topBoundary / itemHeight);
+      const endIndex = Math.ceil(bottomBoundary / itemHeight);
+      return {
+        startIndex: startIndex < 0 ? 0 : startIndex,
+        endIndex: endIndex > length ? length : endIndex,
+        totalHeight: items.length * itemHeight,
+      };
+    },
+    getVariableHeightData() {
+      throw new Error("todo: #getVariableHeightData");
     }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
