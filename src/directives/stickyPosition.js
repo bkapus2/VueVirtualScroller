1edebc695e93d7651ab834c80aa2b4e1a708c404
Unknown <brian.kupi@gmail.com>
2018-02-06 21:07:43 -0600

    lint fixes

--- src/directives/stickyPosition.js
+++ src/directives/stickyPosition.js
@@ -1,75 +1,75 @@
-import isStickySupported from '@/utils/isStickySupported';
-import rafThrottle from '@/utils/rafThrottle';
+// import isStickySupported from '@/utils/isStickySupported';
+// import rafThrottle from '@/utils/rafThrottle';
 
-function bindSticky(el, binding) {
-  const { modifiers } = binding;
-  const { top = false, bottom = false, left = false, right = false } = modifiers;
+// function bindSticky(el, binding) {
+//   const { modifiers } = binding;
+//   const { top = false, bottom = false, left = false, right = false } = modifiers;
 
-  const style = el.style;
-  const { offsetHeight } = el;
-  const containerHeight = el.parentElement.offsetHeight;
+//   const style = el.style;
+//   const { offsetHeight } = el;
+//   const containerHeight = el.parentElement.offsetHeight;
 
-  if (top) {
-    style.top = '0';
-  }
-  if (bottom) {
-    style.top = `${containerHeight - offsetHeight}px`;
-  }
-  if (left) {
-    style.left = '0';
-  }
-  if (right) {
-    style.right = '0';
-  }
-}
+//   if (top) {
+//     style.top = '0';
+//   }
+//   if (bottom) {
+//     style.top = `${containerHeight - offsetHeight}px`;
+//   }
+//   if (left) {
+//     style.left = '0';
+//   }
+//   if (right) {
+//     style.right = '0';
+//   }
+// }
 
-function bindUnsticky(el, binding) {
-  const { modifiers } = binding;
-  const { top = false, bottom = false, left = false, right = false } = modifiers;
+// function bindUnsticky(el, binding) {
+//   const { modifiers } = binding;
+//   const { top = false, bottom = false, left = false, right = false } = modifiers;
 
-  const listener = rafThrottle((event) => {
-    const style = el.style;
-    const { offsetHeight } = el;
-    const containerHeight = el.parentElement.offsetHeight;
+//   const listener = rafThrottle((event) => {
+//     const style = el.style;
+//     const { offsetHeight } = el;
+//     const containerHeight = el.parentElement.offsetHeight;
 
-    console.log(event);
+//     console.log(event);
 
-    if (top) {
-      style.top = '0';
-    }
-    if (bottom) {
-      style.bottom = '0';
-    }
-    if (left) {
-      style.left = '0';
-    }
-    if (right) {
-      style.right = '0';
-    }
-  });
+//     if (top) {
+//       style.top = '0';
+//     }
+//     if (bottom) {
+//       style.bottom = '0';
+//     }
+//     if (left) {
+//       style.left = '0';
+//     }
+//     if (right) {
+//       style.right = '0';
+//     }
+//   });
 
-  const parent = el.parentElement;
-  parent.addEventListener('scroll', listener);
-  binding.unbindListener = function unbindListener() {
-    parent.removeEventListener('scroll', listener);
-  };
-}
+//   const parent = el.parentElement;
+//   parent.addEventListener('scroll', listener);
+//   binding.unbindListener = function unbindListener() {
+//     parent.removeEventListener('scroll', listener);
+//   };
+// }
 
 const stickyPosition = {
-  inserted(el, binding) {
-    if (!isStickySupported) {
-      el.style.position = 'sticky';
-      bindSticky(el, binding);
-    } else {
-      el.style.position = 'absolute';
-      bindUnsticky(el, binding);
-    }
-  },
-  unbind() {
-    if (!isStickySupported) {
-      throw new Error('todo: finish sticky position vue directie');
-    }
-  },
+  // inserted(el, binding) {
+  //   if (!isStickySupported) {
+  //     el.style.position = 'sticky';
+  //     bindSticky(el, binding);
+  //   } else {
+  //     el.style.position = 'absolute';
+  //     bindUnsticky(el, binding);
+  //   }
+  // },
+  // unbind() {
+  //   if (!isStickySupported) {
+  //     throw new Error('todo: finish sticky position vue directie');
+  //   }
+  // },
 };
 
 export default stickyPosition;
